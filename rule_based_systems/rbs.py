2f5058551bd14ed416bf9aea81933fd0286ff6ab
Peter Horvath <horvthpeter@gmail.com>
2014-07-08 00:54:38 +0000

    Create Concept and ConceptManager classes

--- rule_based_systems/rbs.py
+++ rule_based_systems/rbs.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python
 
 
-class Concepts():
+class Rule():
+    pass
+
+
+class KnowledgeBase():
+    pass
+
+
+class ConceptManager():
+    def __init__(self):
+        self.concepts = dict()
+
+    @classmethod
+    def from_file(cls, filename):
+        ''' We initialize from the facts we get. '''
+        new = cls()
+        f = open(filename, "r")
+        for line in f:
+            name, operator, value = line.split(' ')
+            new.concept[name] = Concept(name, value, operator)
+        f.close()
+        return new
+
+
+class Concept():
     def __init__(self, name, value=None, operator=None):
         ''' Initializes the Concept.
 
-        Example concept: name=temperature, value=30, operator=lt, eq, gte etc.
+        Example concept: name=temperature, value=30, operator='<'. '=' etc.
         A value without an operator does not make sense so both or none
         must be present.
         '''
@@ -16,7 +40,8 @@ class Concepts():
 
 
 def main():
-    pass
+    kb = KnowledgeBase.from_file("kb.txt")
+    cm = ConceptManager(kb)
 
 
 if __name__ == "__main__":
